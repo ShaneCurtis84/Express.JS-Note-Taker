5deb964e0f2387faa2a382fd11a0c9c9d2ad4f7d
Shane Curtis <c84a@ymail.com>
2021-07-22 04:17:25 +0000

    Added post functionality

--- server.js
+++ server.js
@@ -3,24 +3,74 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
+const database = require("./db/db")
 
+
+//Express Setup
 const app = express();
 const PORT = 3000;
 
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serving Public Folder
+
+app.use(express.static('public'))
 
 
 // HTML Routes
 
 
-app.get('*', (request, response) => response.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/', (request, response) => response.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.get('/notes', (request, response) => response.sendFile(path.join(__dirname, '/public/notes.html')));
 
 
 // API Routes
 
-//app.get('/api/notes', (request, response))
+// Get notes from the database
+
+app.get('/api/notes', (request, response) => {
+
+    
+    
+    response.json(database)
+
+});
+
+// Post new notes to the database
+
+app.post('/api/notes', (request, response) => {
+   
+      
+    //Variable for to request only body 
+   
+     let createNote = request.body;
+     let dbPath = path.join(__dirname, "/db/db.json");
+
+     // Adds an id to each new note
+
+    createNote.id = uuidv4();
+
+    
+
+
+
+    database.push(createNote)
+
+    fs.writeFile(dbPath, JSON.stringify(database), function (error) {
+
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Note saved!");
+    });
+
+    response.json(database);
+});
 
 
 
